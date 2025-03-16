78108a2455bb828bd6edaa0aa3eeaf906c746352
Umar Khan <muhammed.khan@ucalgary.ca>
2025-03-15 23:11:03 -0600

    fix: adding header to page.js

--- my_portfolio/app/component/header.jsx
+++ my_portfolio/app/component/header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const header = () => {
+const Header = () => {
   return (
     <div>
         <div>
@@ -37,4 +37,4 @@ const header = () => {
   )
 }
 
-export default header
+export default Header
