012c87ce899c45cd975229b01dd45fc77eeeca22
Umar Khan <muhammed.khan@ucalgary.ca>
2025-03-16 04:59:07 -0600

    fix: styling to contact/resume buttons

--- my_portfolio/app/component/header.jsx
+++ my_portfolio/app/component/header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
         I'm a full-stack software engineer with a strong focus on React and modern web development. I have experience working across the stack, building scalable and efficient applications. I'm knowledgeable in both front-end and back-end development, with hands-on experience in various frameworks, languages, and database technologies. I also focus on authentication and security, ensuring applications are well-protected.
         </p>
 
-        <div className="gap-4 mt-4 flex items-center justify-center flex-row sm:flex-col sm:gap-2">
+        <div className="gap-4 mt-4 flex items-center justify-center flex-row">
             <a href="contact" className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2">
                 Contact Me
                 <Image src={assets.right_arrow_white} alt="white right arrow" className="rounded-full w-4" />
