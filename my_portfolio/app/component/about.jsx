b295df7a132906b71f7a85a251e7c7ea26f65c08
Umar Khan <muhammed.khan@ucalgary.ca>
2025-03-16 05:23:18 -0600

    fix: nav bug

--- my_portfolio/app/component/about.jsx
+++ my_portfolio/app/component/about.jsx
@@ -32,10 +32,12 @@ const About = () => {
                 ))}
             </ul>
 
-            <h4>Tools I Use</h4>
-            <ul>
+            <h4 className='my-6 text-gray-700'>Tools I Use</h4>
+            <ul className='flex items-center gap-3 sm:gap-5'>
               {toolsData.map((tool, index) => (
-                <li key={index} className='flex items-center gap-2'>
+                <li className='flex items-center justify-center w-12 sm:w-14 aspect-square border
+                 border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500'
+                  key={index}>
                   <Image src={tool} alt='tool' className='w-5 sm:w-7'/>
                 </li>
               ))}
--- my_portfolio/app/component/navbar.jsx
+++ my_portfolio/app/component/navbar.jsx
@@ -1,10 +1,10 @@
-import React, { useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Image from 'next/image';
 import { assets } from '@/assets/assets';
 
 const Navbar = () => {
     const sideMenuRef = useRef();
-    
+    const [isScroll, setIsScroll] = useState(false);
     const openMenu = () => {
         sideMenuRef.current.style.transform = 'translateX(-16rem)';
     };
@@ -12,14 +12,23 @@ const Navbar = () => {
         sideMenuRef.current.style.transform = 'translateX(16rem)';
     };
 
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if(scrollY > 50) {
+                setIsScroll(true);
+            }else{
+                setIsScroll(false);
+            }
+    })}, []);
+
   return (
     <>
         <div className="fixed top-0 right-0 -z-100 translate-y-[-80%]">
             <Image src={assets.header_bg_color} alt="" className="w-full" />
         </div>
 
-        <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex 
-            items-center justify-between z-50" >
+        <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex 
+            items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm}": ""}`} >
             <a href="#top">
                 <Image 
                 src={assets.logo} 
@@ -28,7 +37,7 @@ const Navbar = () => {
                 />
             </a>
 
-            <ul className='hidden md:flex items-center gap-6 lg:flex rounded-full border-white px-12 py-3 bg-white/50'>
+            <ul className={`hidden md:flex items-center gap-6 lg:flex rounded-full px-12 py-3 ${isScroll ? "":" bg-white shadow-sm bg-opacity-50"}`}>
                 <li><a href="#top">Home</a></li>
                 <li><a href="#about">About me</a></li>
                 <li><a href="#services">Services</a></li>
