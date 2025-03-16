ae4c97e320ceca15e7d9615f8f8a2afc1a4520ec
Umar Khan <muhammed.khan@ucalgary.ca>
2025-03-16 16:14:37 -0600

    fix: adding footer links

--- my_portfolio/app/component/footer.jsx
+++ my_portfolio/app/component/footer.jsx
@@ -8,8 +8,21 @@ const Footer = () => {
         <div className='text-center'>
             <Image src={assets.logo} alt="logo" className="w-36 mx-auto mb-2" />
         </div>
+
+        <div className='w-max flex items-center gap-2 mx-auto'>
+            <Image src={assets.mail_icon} alt="mail icon" className="w-6" />
+            muhammed.khan@ucalgary.ca
+        </div>
+
+        <div className='text-center sm:flx items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
+            <p>&copy; 2025 Muhammed Umar Khan. All rights reserved.</p>
+            <ul className='flex items-center gao-10 justify-center mt-4 sm:mt-0'>
+                <li><a target='_blank' href='https://github.com/Umar5426'>Github</a></li>
+                <li><a target='_blank' href='www.linkedin.com/in/muhammedumarkhan'>LinkedIn</a></li>
+            </ul>
+        </div>
     </div>
   )
-}
+};
 
-export default Footer
+export default Footer;
