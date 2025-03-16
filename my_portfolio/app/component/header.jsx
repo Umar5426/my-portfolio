d55c5d94129454ffc34e3b42624d347a17396207
Umar Khan <muhammed.khan@ucalgary.ca>
2025-03-16 00:44:20 -0600

    fix: finishing header comp

--- my_portfolio/app/component/header.jsx
+++ my_portfolio/app/component/header.jsx
@@ -4,19 +4,24 @@ import Image from 'next/image';
 
 const Header = () => {
   return (
-    <div>
-        <div>
-            <Image src={assets.profile_img} alt='profile image' className='rounded-full w-32'/>
+    <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4 py-20'>
+    
+        <div className="flex items-center gap-4">
+            <div>
+                <Image src={assets.profile_img} alt="profile image" className="rounded-full w-32" />
+            </div>
+            <div>
+                <h3 className="text-xl md:text-2xl mb-3 font-Ovo">
+                    Hi, I'm Umar Khan
+                    <Image src={assets.hand_icon} alt="hand icon" className="rounded-full w-6 inline-block ml-2" />
+                </h3>
+                <h1 className="flex items-center gap-2">
+                    <Image src={assets.location_icon} alt="location icon" className="rounded-full w-6" />
+                    Calgary, AB
+                </h1>
+            </div>
         </div>
 
-        <h3 className='flex item-end gap-2 tex-x1 md:text-2xl mb-3 font-Ovo'>Umar Khan
-            <Image src={assets.hand_icon} alt='profile image' className='rounded-full w-6'/>
-        </h3>
-
-        <h1 className='text-3xl sm:text-6xl lg:text- [66px] font-Ovo'>
-        <Image src={assets.location_icon} alt='profile image' className='rounded-full w-6'/>
-            Calgary, AB
-        </h1>
         <h1 className='text-3xl sm:text-6xl lg:text- [66px] font-Ovo'>
             Full-Stack Software Engineer
         </h1>
@@ -25,16 +30,19 @@ const Header = () => {
         I'm a full-stack software engineer with a strong focus on React and modern web development. I have experience working across the stack, building scalable and efficient applications. I'm knowledgeable in both front-end and back-end development, with hands-on experience in various frameworks, languages, and database technologies. I also focus on authentication and security, ensuring applications are well-protected.
         </p>
 
-        <div className='flex flex-col sm:flex items-center gap-4 mt-4'>
-            <a href="contact" className='px-10 ppy-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>
+        <div className="gap-4 mt-4 flex items-center justify-center sm:flex-col sm:gap-2">
+            <a href="contact" className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2">
                 Contact Me
-                <Image src={assets.right_arrow_white} alt='white right arrow' className='rounded-full w-4'/>
+                <Image src={assets.right_arrow_white} alt="white right arrow" className="rounded-full w-4" />
             </a>
-            <a href="/sample-resume.pdf" download className='px-10 py-4 border rounded-full border-gray-500 flex items-center gap-2'>
+            <a href="/sample-resume.pdf" download className="px-10 py-4 border rounded-full border-gray-500 flex items-center gap-2">
                 My Resumé
-                <Image src={assets.download_icon} alt='download button' className='rounded-full w-4'/>
+                <Image src={assets.download_icon} alt="download button" className="rounded-full w-4" />
             </a>
         </div>
+
+
+
     </div>
   )
 }
