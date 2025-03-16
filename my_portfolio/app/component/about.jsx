67d923f408615d9f29378147d1247f735418a0fa
Umar Khan <muhammed.khan@ucalgary.ca>
2025-03-16 02:17:08 -0600

    fix: day 2 roundup

--- my_portfolio/app/component/about.jsx
+++ my_portfolio/app/component/about.jsx
@@ -23,7 +23,7 @@ const About = () => {
 
             <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
                 {infoList.map(({icon, iconDark, title, description}, index) => (
-                    <li key={index} className='border border-gray-400 rounded-xl p-6 cursor-pointer'>
+                    <li key={index} className='border border-gray-400 rounded-xl p-6 cursor-pointer hover:shadow-lg'>
                         <Image src={icon} alt={title} className='w-7 mt-3'/>
                         <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
                         <p className='text-gray-600 text-sm'>{description}</p>
