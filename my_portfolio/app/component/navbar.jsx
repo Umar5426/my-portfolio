bf0883ebdfc72477fb77e5d9b1b839401619c49c
Umar Khan <muhammed.khan@ucalgary.ca>
2025-03-15 22:49:38 -0600

    add: adding mobile side menu

--- my_portfolio/app/component/navbar.jsx
+++ my_portfolio/app/component/navbar.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Image from 'next/image';
 import { assets } from '@/assets/assets';
 
 const Navbar = () => {
+    const sideMenuRef = useRef();
+    
+    const openMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(-16rem)';
+    };
+    const closeMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(16rem)';
+    };
+
   return (
     <>
     <div className='fixed top-0 right-0 w-11/12 h-1/2'>
@@ -42,10 +51,25 @@ const Navbar = () => {
                         />
                 </a>
 
-                <button className='lg:hidden'>
-                    <Image src={assets.menu_black} alt='' width={30} height={30}/>
+                <button className='block md:hidden ml-3 cursor-pointer' onClick={openMenu}>
+                    <Image src={assets.menu_black} alt='' className='w-6'/>
                 </button>
             </div>
+
+            {/* Mobile Menu */}
+            <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 
+            bottom-0 z-50 h-screen bg-rose-50 transition duration-300'>
+
+                <div style={{ position: 'absolute', top: 10, right: 10 }} onClick={closeMenu}>
+                    <Image src={assets.close_black} alt='' className='cursor-pointer' width={20}/>
+                </div>
+
+                <li><a onClick={closeMenu} href="#top">Home</a></li>
+                <li><a onClick={closeMenu} href="#about">About me</a></li>
+                <li><a onClick={closeMenu} href="#services">Services</a></li>
+                <li><a onClick={closeMenu} href="#work">My Work</a></li>
+                <li><a onClick={closeMenu} href="#contact">Contact Me</a></li>
+            </ul>
         </nav>
     </>
   )
