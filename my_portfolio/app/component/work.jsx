131067f2367c051a9a824868db6c054fc421a750
Umar Khan <muhammed.khan@ucalgary.ca>
2025-03-16 13:30:05 -0600

    fix: auto grid in work page

--- my_portfolio/app/component/work.jsx
+++ my_portfolio/app/component/work.jsx
@@ -12,23 +12,27 @@ const Work = () => {
             I have worked on, ranging from web development to data science.
         </p>
       
-        <div className='grid grid-cols-auto my-10 gap-5'>
+        <div className="grid gap-5" style={{ gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))" }}>
             {workData.map((project, index) => (
-                <div key={index} style={{backgroundImage: `url(${project.bgImage})`}}
-                className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'>
-                    <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 
-                        flex intems-center justify-betwen duration-500 group-hover:bottom-7'>
+                <div key={index}
+                className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'
+                style={{ backgroundImage: `url(${project.bgImage})` }}
+            >
+                    <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 
+                            flex items-center justify-between duration-500 group-hover:bottom-7">
                         <div>
-                            <h2 className='font-semibold'>{project.title}</h2>
-                            <p className='text-sm text-gray-700'>{project.description}</p>
+                            <h2 className="font-semibold">{project.title}</h2>
+                            <p className="text-sm text-gray-700">{project.description}</p>
                         </div>
-                        <div className='border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-rose-50 transition'>
-                            <img src={assets.send_icon} alt='work' className='w-5'/>
+                        <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-rose-50 transition">
+                            <img src={assets.send_icon} alt="work" className="w-5" />
                         </div>
                     </div>
                 </div>
             ))}
         </div>
+
+
 
     </div>
   )
