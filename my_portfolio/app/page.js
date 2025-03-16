083817c7fd5b93062c2f30f5e7f1f640219ece5d
Umar Khan <muhammed.khan@ucalgary.ca>
2025-03-16 00:46:10 -0600

    add: adding about comp

--- my_portfolio/app/page.js
+++ my_portfolio/app/page.js
@@ -1,12 +1,14 @@
 'use client'
 import Navbar from "./component/navbar"
 import Header from "./component/header"
+import About from "./component/about";
 
 export default function Home() {
   return (
     <>
       <Navbar/>
       <Header/>
+      <About/>
     </>
   );
 }
