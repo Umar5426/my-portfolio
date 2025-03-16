64d5b682135398e554c6a194c0481605dc601798
Umar Khan <muhammed.khan@ucalgary.ca>
2025-03-16 15:56:53 -0600

    add: adding footer comp

---
+++ my_portfolio/app/component/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image';
+import { assets } from '@/assets/assets';
+
+const Footer = () => {
+  return (
+    <div className='mt-20'>
+        <div className='text-center'>
+            <Image src={assets.logo} alt="logo" className="w-36 mx-auto mb-2" />
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- my_portfolio/app/page.js
+++ my_portfolio/app/page.js
@@ -4,6 +4,7 @@ import Header from "./component/header"
 import About from "./component/about";
 import Work from "./component/work";
 import Contact from "./component/contact";
+import Footer from "./component/footer";
 
 export default function Home() {
   return (
@@ -12,7 +13,8 @@ export default function Home() {
       <Header/>
       <About/>
       <Work/>
-      <Contact />
+      <Contact/>
+      <Footer/>
     </>
   );
 }
