67bdbd1bed9e4c399d8ce30b3efe31c99b75142e
Umar Khan <muhammed.khan@ucalgary.ca>
2025-03-18 14:14:07 -0600

    fix: fixing font family

--- my_portfolio/app/layout.js
+++ my_portfolio/app/layout.js
@@ -1,11 +1,11 @@
 import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
 
-const outfit = Outfit({
+export const outfit = Outfit({
   subsets: ["latin"], weight: ["400", "500", "600", "700"]
 });
 
-const ovo = Ovo({
+export const ovo = Ovo({
   subsets: ["latin"], weight: ["400"]
 });
 
@@ -17,8 +17,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body
-        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-[#11001f] dark:text-white`}>
+    <body style={{fontFamily: `${outfit.style.fontFamily}, ${ovo.style.fontFamily}`}}
+    className="antialiased leading-8 overflow-x-hidden dark:bg-[#11001f] dark:text-white">
+
         {children}
       </body>
     </html>
