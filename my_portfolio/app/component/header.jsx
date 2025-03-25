c749cac4a5c7aedbee9d78fc75974faba653b8f8
Umar Khan <muhammed.khan@ucalgary.ca>
2025-03-25 01:30:15 -0600

    fix: header and navbar interaction bug

--- my_portfolio/app/component/header.jsx
+++ my_portfolio/app/component/header.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 import { assets } from '@/assets/assets';
 import Image from 'next/image';
 
-const Header = (isDarkMode) => {
+const Header = ({ isDarkMode }) => {
   return (
-    <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4 py-20'>
+    <div className='relative z-10 w-11/12 max-w-3xl text-center mx-auto min-h-screen flex flex-col items-center justify-center gap-4 p-20 pt-24'>
     
         <div className="flex items-center gap-4">
             <div>
                 <Image src={assets.profile_img} alt="profile image" className="rounded-full w-32" />
             </div>
             <div>
-                <h3 className={`text-xl md:text-2xl mb-3 `}>
+                <h3 className="text-xl md:text-2xl mb-3">
                     Hi, I'm Umar Khan
                     <Image src={assets.hand_icon} alt="hand icon" className="rounded-full w-6 inline-block ml-2" />
                 </h3>
                 <h1 className="flex items-center gap-2">
-                    <Image src={isDarkMode? assets.location_icon_dark : assets.location_icon} alt="location icon" className="rounded-full w-6" />
+                    <Image src={isDarkMode ? assets.location_icon_dark : assets.location_icon} alt="location icon" className="rounded-full w-6" />
                     Calgary, AB
                 </h1>
             </div>
         </div>
 
-        <h1 className='text-3xl sm:text-6xl lg:text- [66px] font-Outfit'>
+        <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Outfit'>
             Full-Stack Software Engineer
         </h1>
 
@@ -37,14 +37,12 @@ const Header = (isDarkMode) => {
             </a>
             <a href="/UmarKhanResume.pdf" download className="px-10 py-4 border rounded-full border-gray-500 flex items-center gap-2 dark:bg-white dark:text-black">
                 My Resumé
-                <Image src={assets.download_icon} alt="download button" className="rounded-50 w-4" />
+                <Image src={assets.download_icon} alt="download button" className="rounded-full w-4" />
             </a>
         </div>
-
-
 
     </div>
   )
 }
 
-export default Header
+export default Header;
